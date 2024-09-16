ab9442f601e046d047fa443a69341b49efd6b85e
locleabcd <locleabcd@gmail.com>
2024-09-16 17:12:06 +0700

    empty application

---
+++ testing_azure/src/test/java/com/example/testing_azure/TestingAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
